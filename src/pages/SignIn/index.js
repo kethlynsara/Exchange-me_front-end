714e65d2e4a115b8e8aea7283259d0a3d416fa06
Kethlyn <kethlynsaraa@gmail.com>
2022-08-04 03:58:26 -0300

    feat: sign in

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,5 +1,37 @@
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import axios from "axios";
+
 function SignIn() {
-    return <h1>I'm sign in page</h1>
+    const URL = "http://localhost:5000";
+    const [data, setData] = useState({
+        email: "",
+        password: "",
+    });
+    const navigate = useNavigate();
+
+    async function login(e) {
+        e.preventDefault();
+        console.log("data", data)
+
+        try {
+            const {data: userInfo} = await axios.post(`${URL}/signin`, data);
+            console.log(userInfo);
+            const serializedData = JSON.stringify(userInfo);
+            localStorage.setItem("userInfo", serializedData);
+            navigate("/");
+        } catch (error) {
+            console.log(e.response.data.error)
+        }
+    }
+
+    return <>
+        <form onSubmit={login}>
+            <input type="email" placeholder="Email" required value={data.email} onChange={e => setData({...data, email: e.target.value})} />
+            <input type="password" placeholder="Password" required value={data.password} onChange={e => setData({...data, password: e.target.value})} />
+            <button type="submit">Sign In</button>
+        </form>
+    </>
 }
 
 export default SignIn;
